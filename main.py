886da72285ce47c33559dc59d44787aad29bce52
Tony Tran <thanhtan.tran@gmail.com>
2025-03-02 22:59:59 +0700

    main and config

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 # from flask_script import Manager
 from controller import create_app
 
-# 创建APP对象,使用参数dev时开启debug
 # app = create_app('dev')
 app = create_app('pro')
-# # 创建脚本管理
-# mgr = Manager(app)
 
 
 if __name__ == '__main__':
